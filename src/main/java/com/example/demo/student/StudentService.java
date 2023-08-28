44d0abeb25b54bea171228cde1befe4f6c20cf0b
davidgalfi <galfidavid314@gmail.com>
2023-08-28 09:45:14 +0200

    # Create StudentService and implement JPA annotations

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -8,13 +9,15 @@ import java.util.List;
 
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents(){
-        return List.of(new Student(
-                1L,
-                "David",
-                "david@gmail.com",
-                LocalDate.of(2002, Month.JUNE, 8),
-                21
-        ));
+        return studentRepository.findAll();
     }
 }
